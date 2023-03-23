9fd3e7db5be7d59f707c3d7aedec08a2e9c4caf6
Jackie Yang <jackie@jackieyang.me>
2023-03-23 16:29:34 -0700

    Added blinky for arduino ble 33 sense v2

--- app/src/main.c
+++ app/src/main.c
@@ -5,43 +5,31 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
 
 #include "app_version.h"
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-void main(void)
-{
-	int ret;
-	const struct device *sensor;
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL
+);
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
+void main(void) {
+    int ret;
+    const struct device *sensor;
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return;
-	}
+    printk("Zephyr Example Application %s\n", APP_VERSION_STR);
 
-	while (1) {
-		struct sensor_value val;
+    const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
 
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return;
-		}
+    while (1) {
+        // blink
+        gpio_pin_set_dt(&led, 1);
 
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
+        k_sleep(K_MSEC(1000));
 
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
-	}
+        gpio_pin_set_dt(&led, 0);
+        k_sleep(K_MSEC(1000));
+    }
 }
 
