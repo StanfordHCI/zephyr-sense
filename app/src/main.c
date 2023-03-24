1551ebc3a8a1e2d5ee3e687c88fbee69068daaf6
Jackie Yang <jackie@jackieyang.me>
2023-03-23 17:24:05 -0700

    Added serial and clion config

--- app/src/main.c
+++ app/src/main.c
@@ -1,35 +1,45 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
 
 #include "app_version.h"
 
-#include <zephyr/logging/log.h>
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+             "Console device is not ACM CDC UART device");
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL
-);
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-void main(void) {
-    int ret;
-    const struct device *sensor;
+void main(void)
+{
+    const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+    uint32_t dtr = 0;
+
+    if (usb_enable(NULL)) {
+        return;
+    }
+
+    /* Poll if the DTR flag was set */
+    while (!dtr) {
+        uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+        /* Give CPU resources to low priority threads. */
+        k_sleep(K_MSEC(100));
+    }
 
     printk("Zephyr Example Application %s\n", APP_VERSION_STR);
 
     const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
+    int led_on = 0;
 
     while (1) {
-        // blink
-        gpio_pin_set_dt(&led, 1);
+        printk("Hello World! %s\n", CONFIG_ARCH);
 
-        k_sleep(K_MSEC(1000));
+        gpio_pin_set_dt(&led, led_on);
+        led_on = !led_on;
 
-        gpio_pin_set_dt(&led, 0);
-        k_sleep(K_MSEC(1000));
+        k_sleep(K_SECONDS(1));
     }
 }
-
